bc58e82fc41e64729c6d967cce82ecbac1e169fc
Vladimir Dementyev <dementiev.vm@gmail.com>
2020-06-12 21:48:15 +0300

    Make take_screenshot preserver current session for failures

--- spec/support/session_helpers.rb
+++ spec/support/session_helpers.rb
@@ -2,7 +2,17 @@
 
 module SessionHelpers
   def within_session(name, &block)
-    Capybara.using_session(name, &block)
+    Capybara.using_session(name) do
+      @last_active_session = name
+      block.call
+      @last_active_session = nil
+    end
+  end
+
+  # Make failure screenshots compatible with multi-session setup
+  def take_screenshot
+    return super unless @last_active_session
+    Capybara.using_session(@last_active_session) { super }
   end
 
   def mark_all_banners_as_read!
