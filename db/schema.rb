d352fdded0b4767cfc5d81362d1439f35007c892
Vladimir Dementyev <dementiev.vm@gmail.com>
2020-06-16 21:00:05 +0300

    Migrate to sqlite, drop Redis
    That would simplify running the application on a host machine

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,8 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.0].define(version: 2020_05_04_092152) do
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
   create_table "items", force: :cascade do |t|
-    t.bigint "list_id", null: false
+    t.integer "list_id", null: false
     t.text "desc", null: false
     t.boolean "completed", default: false, null: false
     t.datetime "created_at", null: false
@@ -25,7 +22,7 @@ ActiveRecord::Schema[7.0].define(version: 2020_05_04_092152) do
 
   create_table "lists", force: :cascade do |t|
     t.string "name", null: false
-    t.bigint "workspace_id", null: false
+    t.integer "workspace_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["workspace_id"], name: "index_lists_on_workspace_id"
