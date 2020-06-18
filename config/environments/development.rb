a8360450035b72aabdbaf4909ca2392c71754bd8
Vladimir Dementyev <dementiev.vm@gmail.com>
2020-06-18 14:34:52 +0300

    Setup AnyCable
    This commit contains the changes after running `rails g anycable:setup`

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,6 +1,10 @@
 # frozen_string_literal: true
 
 Rails.application.configure do
+  # Specify AnyCable WebSocket server URL to use by JS client
+  config.after_initialize do
+    config.action_cable.url = ActionCable.server.config.url = ENV.fetch("CABLE_URL", "ws://localhost:8080/cable") if AnyCable::Rails.enabled?
+  end
   # Settings specified here will take precedence over those in config/application.rb.
 
   # In the development environment your application's code is reloaded on
