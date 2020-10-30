f844427ba4f9a1b577ef160aa3b3b63d083c8335
Vladimir Dementyev <dementiev.vm@gmail.com>
2020-10-30 18:50:02 +0300

    Configure cache headers for assets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -31,6 +31,10 @@ Rails.application.configure do
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
   config.public_file_server.enabled = ENV["RAILS_SERVE_STATIC_FILES"].present?
+  config.public_file_server.headers = {
+    "Cache-Control" => "public, s-maxage=31536000, max-age=15552000",
+    "Expires" => 1.year.from_now.to_formatted_s(:rfc822)
+  }
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.action_controller.asset_host = 'http://assets.example.com'
