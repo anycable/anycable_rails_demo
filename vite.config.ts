226d400503f3589d00d9ba68c673505a10d06988
Vladimir Dementyev <dementiev.vm@gmail.com>
2022-06-27 20:02:02 -0400

    dev: configure vite plugins

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
-import { defineConfig } from 'vite'
-import RubyPlugin from 'vite-plugin-ruby'
+import { defineConfig } from "vite";
+import RubyPlugin from "vite-plugin-ruby";
+import StimulusHMR from "vite-plugin-stimulus-hmr";
+import FullReload from "vite-plugin-full-reload";
 
 export default defineConfig({
-  plugins: [
-    RubyPlugin(),
-  ],
-})
+  plugins: [RubyPlugin(), StimulusHMR(), FullReload(["app/views/**/*.erb"])],
+});
