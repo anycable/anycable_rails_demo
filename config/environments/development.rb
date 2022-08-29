2e31e9a6b7f2a1a5b9fc9033ebcb53537c0dc8cf
Vladimir Dementyev <palkan@hey.com>
2022-08-29 17:46:04 -0400

    fixup! Instrumentation and monitoring

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -46,9 +46,5 @@ Rails.application.configure do
   # Raises error for missing translations.
   # config.action_view.raise_on_missing_translations = true
 
-  # Use an evented file watcher to asynchronously detect changes in source code,
-  # routes, locales, etc. This feature depends on the listen gem.
-  config.file_watcher = ActiveSupport::EventedFileUpdateChecker
-
   config.log_level = :warn if ENV["ANYCABLE_DEBUG"] == "0"
 end
