683eae637f0e84ae47786328d761ff1aea342fac
Paulo Santos <paulo.santos@deemaze.com>
2025-01-02 18:54:30 +0000

    Add healthcheck route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  # App boot health check
+  get '/up', to: 'rails/health#show', as: :rails_health_check
+
   get "/login", to: "sessions#new", as: :login
   post "/login", to: "sessions#create"
   delete "/logout", to: "sessions#destroy", as: :logout
